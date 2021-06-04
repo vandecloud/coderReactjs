ce80fe583192eac958e85f1a54cf411913def0dc
p-vande <ext_pavandec@mercadolibre.com>
2021-06-03 22:20:40 -0300

    Creacion primer componente NavBar

---
+++ vande-tienda/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {AppBar, Toolbar, Typography} from '@material-ui/core'
+
+const Navbar = () => {
+    return(
+        <div>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant = 'h6'>
+                        Desarollando mi Navbar
+
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Navbar
